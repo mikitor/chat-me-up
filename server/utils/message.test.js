b0c215d3d3af16cec3b821fc2bff17331fdd1588
Nick Torbich <nicktorbich@protonmail.com>
2019-01-03 17:11:59 +0100

    Add test for generateLocationMsg utility

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,5 +1,5 @@
 const expect = require('expect');
-const { generateMsg } = require('./message');
+const { generateMsg, generateLocationMsg } = require('./message');
 
 describe('generateMsg', () => {
   it('should generate a message object', () => {
@@ -8,6 +8,21 @@ describe('generateMsg', () => {
     expect(typeof result).toBe('object');
     expect(result.text).toBe('Test');
     expect(result.from).toBe('Admin');
+    expect(typeof result.createdAt).toBe('number');
+  });
+});
+
+describe('generateLocationMsg', () => {
+  it('should generate a location object', () => {
+    const from = 'Ad';
+    const lat = '47';
+    const lng = '12';
+    const url = 'https://www.google.com/maps/search/?api=1&query=47,12';
+    const result = generateLocationMsg(from, lat, lng);
+
+    expect(typeof result).toBe('object');
+    expect(result.from).toBe(from);
+    expect(result.url).toBe(url);
     expect(typeof result.createdAt).toBe('number');
   });
 });
